64ed2842e9cb636dc2955ed220c94a43ad43723d
LuigiM <jlpadilla@innovacion.gob.sv>
2025-04-24 16:00:28 -0600

    Feat(HomeView): Avance de seccion

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,4 +1,5 @@
 import { en as navEn, es as navEs} from './translate/nav.json'
+import { enHome, esHome} from './translate/views/home.json'
 
 export const languages = {
     en: 'English',
@@ -9,9 +10,11 @@ export const defaultLang = 'es';
 export const ui = {
     es: {
         ...navEs,
+        ...esHome,
     },
     en: {
         ...navEn,
+        ...enHome,
 
     },
 } as const;
