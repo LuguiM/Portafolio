64a53fd8fbb6bacb495571bef2231b1d70c9b559
LuigiM <jlpadilla@innovacion.gob.sv>
2025-04-28 15:18:14 -0600

    Feat: Seccion tecnologias

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -1,6 +1,7 @@
 import { en as navEn, es as navEs} from './translate/nav.json'
 import { enHome, esHome} from './translate/views/home.json'
 import { enExperience, esExperience} from './translate/views/experience.json'
+import { enTech, esTech } from './translate/views/tech.json'
 
 export const languages = {
     en: 'English',
@@ -13,11 +14,12 @@ export const ui = {
         ...navEs,
         ...esHome,
         ...esExperience,
+        ...esTech
     },
     en: {
         ...navEn,
         ...enHome,
         ...enExperience,
-
+        ...enTech
     },
 } as const;
