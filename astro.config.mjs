800d4e806cbe57da86f8a400a2becd9b50d5a3cd
LuigiM <jlpadilla@innovacion.gob.sv>
2025-04-29 10:51:33 -0600

    Feat: Configarion de ruta

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://luguim.github.io',
+  base: 'Portafolio',
   vite: {
     plugins: [tailwindcss()]
   },
